fe4b8bc408e7a33d883348e8ea1685d725996d8e
shj <kan02134@gmail.com>
2020-09-03 17:27:11 +0900

    Implement InfoMsg component

---
+++ src/components/InfoMsg.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="info-msg">
+    <p>
+      <a target="_blank" href="https://github.com/Gumball12/tech-interview-quiz">@GitHub</a>
+    </p>
+    <p>
+      <a target="_blank" href="https://github.com/Gumball12/tech-interview-quiz-items">@Add-Items</a>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+div.info-msg {
+  position: fixed;
+
+  top: 1em;
+  right: 1em;
+}
+
+div.info-msg > p {
+  margin: 0;
+  font-size: 12px;
+}
+
+div.info-msg > p > a {
+  color: black;
+}
+</style>
