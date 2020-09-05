4e5ed37167821fa186fa04c6bfd7b5b4e67cc754
shj <kan02134@gmail.com>
2020-09-05 16:42:43 +0900

    Refactor html component

--- src/components/InfoMsg.vue
+++ src/components/InfoMsg.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="info-msg">
+  <header>
     <p>
       <a target="_blank" href="https://github.com/Gumball12/tech-interview-quiz">@GitHub</a>
     </p>
     <p>
       <a target="_blank" href="https://github.com/Gumball12/tech-interview-quiz-items">@Add-Items</a>
     </p>
-  </div>
+  </header>
 </template>
 
 <style scoped>
-div.info-msg {
+header {
   position: fixed;
 
   top: 1em;
   right: 1em;
 }
 
-div.info-msg > p {
+header > p {
   margin: 0;
   font-size: 12px;
 }
 
-div.info-msg > p > a {
+header > p > a {
   color: black;
 }
 </style>
