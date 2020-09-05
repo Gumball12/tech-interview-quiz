6c1164cb6c6e801a6d2ae2f43bfd3c907ef26754
shj <kan02134@gmail.com>
2020-09-05 11:29:28 +0900

    Add code element style

--- src/components/QuizCard.vue
+++ src/components/QuizCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="card">
-    <p class="title" v-text="title" />
+    <p class="title" v-html="title" />
 
     <button v-if="!isAnswerOpen"
       @click="isAnswerOpen = true">답 확인</button>
 
     <template v-else>
       <hr>
-      <div v-html="answer_md"/>
+      <div v-html="answer"/>
       <button @click="window.location.reload()">다른 문제</button>
     </template>
   </div>
@@ -24,16 +24,19 @@ function getRandomElement(arr) {
 
 export default {
   data: () => ({
-    title: '',
-    answer: '',
+    titleRaw: '',
+    answerRaw: '',
     isAnswerOpen: false,
   }),
   computed: {
     window() {
       return window;
     },
-    answer_md() {
-      return new MarkdownIt().render(this.answer);
+    answer() {
+      return new MarkdownIt().render(this.answerRaw);
+    },
+    title() {
+      return new MarkdownIt().render(this.titleRaw);
     },
   },
   async mounted() {
@@ -49,7 +52,7 @@ export default {
     const data = yaml.parse(raw);
     const randomData = getRandomElement(data);
 
-    [this.title, this.answer] = randomData;
+    [this.titleRaw, this.answerRaw] = randomData;
   },
 };
 </script>
@@ -71,6 +74,15 @@ div.card {
 
 div.card > p.title {
   font-size: 32px;
+}
+
+div.card ::v-deep code {
+  background: #eee;
+  color: #ff7f00;
+
+  padding: 1px;
+  padding-left: 2px;
+  padding-right: 2px;
 }
 
 div.card button {
