8e8f694a381c657775d0c4932cc17cc3b99eaeb5
shj <kan02134@gmail.com>
2020-09-06 13:47:52 +0900

    Implement import quiz code

--- src/components/QuizCardRenderer.vue
+++ src/components/QuizCardRenderer.vue
@@ -15,6 +15,32 @@ import yaml from 'yaml';
 
 import QuizCard from './QuizCard.vue';
 
+const base = process.env.NODE_ENV === 'production' ? 'https://raw.githubusercontent.com/Gumball12/tech-interview-quiz-items/master' : '';
+
+async function recursiveImport(raw) {
+  const importStmts = raw.match(/^-[\s]*?\${.*?}$/gm);
+
+  if (importStmts === null) {
+    return raw;
+  }
+
+  await new Promise((resolve) => {
+    importStmts.forEach(async (importStmt, ind) => {
+      const importSrc = `${base}${importStmt.match(/\${(.*?)}/)[1]}`;
+      const importRaw = await (await fetch(importSrc)).text();
+
+      // eslint-disable-next-line
+      raw = raw.replace(importStmt, importRaw);
+
+      if (ind === importStmts.length - 1) {
+        resolve();
+      }
+    });
+  });
+
+  return recursiveImport(raw);
+}
+
 export default {
   props: {
     isAllLoad: {
@@ -33,15 +59,9 @@ export default {
     },
   },
   async mounted() {
-    let quizSrc;
-
-    if (process.env.NODE_ENV === 'production') {
-      quizSrc = 'https://raw.githubusercontent.com/Gumball12/tech-interview-quiz-items/master/quiz.yml';
-    } else if (process.env.NODE_ENV === 'development') {
-      quizSrc = '/quiz.yml';
-    }
-
-    const raw = await (await fetch(quizSrc)).text();
+    // eslint-disable-next-line
+    const srcRaw = '- ${/quiz.yml}';
+    const raw = await recursiveImport(srcRaw);
     this.data = yaml.parse(raw);
   },
 };
